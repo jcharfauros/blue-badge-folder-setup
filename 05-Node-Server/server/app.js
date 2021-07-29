27a6bdbec3b70047a7104c188c446f70db1456a8
jcharfauros <julia.charfauros@gmail.com>
2021-07-29 12:56:24 -0400

    module 9.2.3 user table created

--- 05-Node-Server/server/app.js
+++ 05-Node-Server/server/app.js
@@ -2,11 +2,14 @@ let express = require('express');
 let app = express();
 let sequelize = require('./db');
 
-let user = require('./controllers/usercontroller');
 let journal = require('./controllers/journalcontroller');
+let user = require('./controllers/usercontroller');
 
 sequelize.sync();
 //sequelize.sync({forceL true})
+
+app.use(express.json());
+//tells the app that we want json to be used when request is processed
 
 app.use('/user', user);
 app.use('/journal', journal);
--- 05-Node-Server/server/controllers/usercontroller.js
+++ 05-Node-Server/server/controllers/usercontroller.js
@@ -1,4 +1,17 @@
 const router = require('express').Router();
 const User = require('../db').import('../models/user');
 
+/* USER SIGNUP */
+router.post('/create', function(req, res) {
+
+    User.create({
+        email: req.body.user.email,
+        password: req.body.user.password
+    })
+        .then(
+            res.send("This is our user/create endpoint!")
+        );
+});
+
+
 module.exports = router;
