eae31b53c3cfdf70c05ed4c5ae7b9b1331296e8e
jcharfauros <julia.charfauros@gmail.com>
2021-07-29 17:06:02 -0400

    module 9.3.6 user login/error handling live

--- 05-Node-Server/server/controllers/usercontroller.js
+++ 05-Node-Server/server/controllers/usercontroller.js
@@ -18,5 +18,24 @@ router.post('/create', function(req, res) {
         .catch(err=> res.status(500).json({error: err}))
 });
 
+/* USEAR SIGNIN */
+router.post('/login', function(req, res) {
+    User.findOne({
+        where: {
+            email: req.body.user.email
+        }
+    })
+        .then(function loginSuccess(user) {
+            if (user) {
+                res.status(200).json({
+                    user: user            
+            })
+        }   else {
+        res.status(500).json({ error: 'User does not exist.'})        
+        }
+    })
+    .catch(err => res.status(500).json({ error: err }))
+});
+
 
 module.exports = router;
