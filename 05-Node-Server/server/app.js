28d9b49d6e2720e57cad5f2812081677a75d6078
jcharfauros <julia.charfauros@gmail.com>
2021-07-28 23:53:13 -0400

    module 4.2

--- 05-Node-Server/server/app.js
+++ 05-Node-Server/server/app.js
@@ -1,9 +1,13 @@
 let express = require('express');
 let app = express();
+let journal = require('./controllers/journalcontroller')
 
-app.use('/test', function(req, res){
-    res.send('This is a message from the test endpoint on the server!')
-})
+//test code from set up
+// app.use('/test', function(req, res){
+//     res.send('This is a message from the test endpoint on the server!')
+// })
+
+app.use('/journal', journal)
 
 app.listen(3000, function(){
     console.log("App is listening on port 3000");
