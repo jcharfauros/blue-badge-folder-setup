cfb6dfe119fee4deacfab29869af6e021b882a8c
jcharfauros <julia.charfauros@gmail.com>
2021-07-29 17:41:42 -0400

    module 10.6 JWT challenge solution

--- 05-Node-Server/server/controllers/usercontroller.js
+++ 05-Node-Server/server/controllers/usercontroller.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../db').import('../models/user');
+const jwt = require("jsonwebtoken");
 
 /* USER SIGNUP */
 router.post('/create', function(req, res) {
@@ -10,8 +11,12 @@ router.post('/create', function(req, res) {
     })
         .then(
             function createSuccess(user){
+                let token = jwt.sign({id: user.id}, "i_am_secret", {expiresIn: 60 * 60 * 24});
+
                 res.json({
-                    user: user
+                    user: user,
+                    message: 'User successfully created!',
+                    sessionToken: token
                 });
             }
         )
