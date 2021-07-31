aef3dbe760c75a2f250452ff26266f08eafadbea
jcharfauros <julia.charfauros@gmail.com>
2021-07-31 00:15:01 -0400

    bcrypt modules completed, bcrypt is functional

--- 05-Node-Server/server/controllers/usercontroller.js
+++ 05-Node-Server/server/controllers/usercontroller.js
@@ -1,13 +1,15 @@
 const router = require('express').Router();
 const User = require('../db').import('../models/user');
 const jwt = require("jsonwebtoken");
+const bcrypt = require('bcryptjs');
 
 /* USER SIGNUP */
 
 router.post('/create', function(req, res) {
     User.create({
         email: req.body.user.email,
-        password: req.body.user.password,
+        // password: req.body.user.password, //OLD w/o bcrypt
+        password: bcrypt.hashSync(req.body.user.password, 13),
     })
         .then(function createSuccess(user){
                 // code for token creation //payload    //signature
@@ -34,20 +36,26 @@ router.post('/login', function(req, res) {
     })
         .then(function loginSuccess(user) {
             if (user) {
-                //same as signup token, except for 200 msg
-                // let token = jwt.sign({id: user.id}, "i_am_secret", {expiresIn: 60 * 60 * 24}) //OLD CODE
-                const token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: 60 * 60 * 24});
-
-                res.status(200).json({
-                    user: user,
-                    message: "User successfully logged in!",
-                    sessionToken: token            
-            })
-        }   else {
-        res.status(500).json({ error: 'User does not exist.'});        
-        }
-    })
-    .catch(err => res.status(500).json({ error: err }));
+                bcrypt.compare(req.body.user.password, user.password, function(err, matches) {
+                    if(matches){
+                        //same as signup token, except for 200 msg
+                        // let token = jwt.sign({id: user.id}, "i_am_secret", {expiresIn: 60 * 60 * 24}) //OLD CODE
+                        const token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: 60 * 60 * 24});
+                        
+                        res.status(200).json({
+                            user: user,
+                            message: "User successfully logged in!",
+                            sessionToken: token            
+                    })
+                } else {
+                    res.status(502).send({error: "Login Failed"});
+                }
+            });
+            } else {
+                res.status(500).json({ error: 'User does not exist.'}); 
+            }
+        })
+        .catch(err => res.status(500).json({ error: err }));
 });
 
 
