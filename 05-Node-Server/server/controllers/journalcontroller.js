5868968f775fd9827a58a39bd3f9a4d4ac13f719
jcharfauros <julia.charfauros@gmail.com>
2021-08-02 23:04:12 -0400

    Modules 12 completed/functional

--- 05-Node-Server/server/controllers/journalcontroller.js
+++ 05-Node-Server/server/controllers/journalcontroller.js
@@ -21,9 +21,61 @@ router.post('/create', validateSession, (req, res) => {
         .catch(err => res.status(500).json({ error: err }));
 })
 
-router.get('/about', function(req, res)
-{
-    res.send('This is the about route')
-})
+/* GET ALL ENTRIES */
+router.get("/", (req, res) => {
+    Journal.findAll()
+        .then(journals => res.status(200).json(journals))
+        .catch(err => res.status(500).ison({ error: err }))
+});
+
+/* GET ENTRIES BY USER */
+router.get("/mine", validateSession, (req, res) => {
+    let userid = req.user.id
+    Journal.findAll({
+        where: { owner: userid }
+    })
+        .then(journals => res.status(200).json(journals))
+        .catch(err => res.status(500).json({ error: err }))
+});
+
+/* GET ENTRIES BY TITLE */
+router.get("/:title", function(req, res) {
+    let title = req.params.title;
+
+    Journal.findAll({
+        where: { title: title }
+    })
+        .then(journals => res.status(200).json(journals))
+        .catch(err => res.status(500).json({ error: err }))
+});
+
+/* UPDATE (PUT) ENTRIES */
+router.put("/update/:entryId", validateSession, function(req, res) {
+    const updateJournalEntry = {
+        title: req.body.journal.title,
+        date: req.body.journal.date,
+        entry: req.body.journal.entry
+    };
+
+    const query = { where: { id: req.params.entryId, owner: req.user.id }};
+
+    Journal.update(updateJournalEntry, query)
+        .then((journals) => res.status(200).json(journals))
+        .catch((err) => res.status(500).json({ error: err }));
+});
+
+/* DELETE ENTRY */
+router.delete("/delete/:id", validateSession, function(req, res) {
+    const query = { where: { id: req.params.id, owner: req.user.id }};
+
+    Journal.destroy(query)
+        .then(() => res.status(200).json({ message: "Journal Entry Removed" }))
+        .catch((err) => res.status(500).json({ error: err }));
+});
+
+// router.get('/about', function(req, res)
+// {
+//     res.send('This is the about route')
+// })
 
 module.exports = router;
